779f9d8d97fa32556f6125b6529cd7f502ccfd5d
Ekaterina Zhitkevich <zhitkevich.ekaterina@yandex.ru>
2023-11-20 20:43:17 +0300

    add contoller package, @Transactional

--- src/main/java/rest/simplerest/service/impl/FlowersServiceImpl.java
+++ src/main/java/rest/simplerest/service/impl/FlowersServiceImpl.java
@@ -3,6 +3,7 @@ package rest.simplerest.service.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import rest.simplerest.entity.Flower;
 import rest.simplerest.repository.FlowerRepository;
 import rest.simplerest.service.FlowerService;
@@ -17,6 +18,7 @@ public class FlowersServiceImpl implements FlowerService {
     private final FlowerRepository flowerRepository;
 
     @Override
+    @Transactional
     public List<Flower> getAllFlowers() {
         log.info("Method get all flowers in class {} started", getClass().getSimpleName());
         List<Flower> flowers = flowerRepository.findAll();
@@ -26,6 +28,7 @@ public class FlowersServiceImpl implements FlowerService {
 
 
     @Override
+    @Transactional
     public List<Flower> getFlowersByType(String type) {
         log.info("Method get flowers by type in class {} started", getClass().getSimpleName());
         List<Flower> flowers = flowerRepository.findFlowerByType(type);
@@ -34,6 +37,7 @@ public class FlowersServiceImpl implements FlowerService {
     }
 
     @Override
+    @Transactional
     public void saveFlower(Flower flower) {
         log.info("Method save flower in class {} started", getClass().getSimpleName());
         flowerRepository.save(flower);
